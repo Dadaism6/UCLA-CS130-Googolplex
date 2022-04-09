0ab303add1cd067e3fc1af490134001bea6fe2c4
段辰达 <chenda@g.ucla.edu>
2022-04-09 21:50:31 +0000

    handle multiple request
    Change-Id: Ib957ac22af913b36760e8bcc63377a3ab1ef3dbe

--- src/session.cc
+++ src/session.cc
@@ -49,6 +49,7 @@ void session::handle_read(const boost::system::error_code& error,
             boost::asio::placeholders::error));
     }
     else {
+      memset(in_data_, 0, max_length);
       socket_.async_read_some(boost::asio::buffer(in_data_, max_length),
         boost::bind(&session::handle_read, this,
           boost::asio::placeholders::error,
@@ -66,6 +67,9 @@ void session::handle_write(const boost::system::error_code& error)
   if (!error)
   {
     memset(out_data_, 0, max_length + 46); // reset out buffer
+    memset(response_header_, 0, header_length + 1); // reset response header
+    next_out_data_fill_pos = header_length;
+    strcpy(response_header_, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n");
     socket_.async_read_some(boost::asio::buffer(in_data_, max_length),
         boost::bind(&session::handle_read, this,
           boost::asio::placeholders::error,
