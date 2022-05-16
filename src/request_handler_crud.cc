6c03eb8ab35535fac13e13c2bf14c9d1a1b9a27d
shammo <shammochanda@g.ucla.edu>
2022-05-16 15:13:59 -0700

    Added PUT functionality to CRUD API:
    -Added crud_data folder
    
    -If entity does not exist, it creates it with the corresponding id and returns
    HTTP 201 message
    
    -If entity exists but id does not, it creates it with the corresponding id and
    returns HTTP 201 message
    
    -If entity exists and id exists, it overwrites the current content of the id
    
    Change-Id: I46c748bb843783473d0a08af6a86166f10c5c4f7

--- src/request_handler_crud.cc
+++ src/request_handler_crud.cc
@@ -26,9 +26,6 @@ status request_handler_crud::handle_request(http::request<http::string_body> req
                 }
                 //if entity does not exist yet
                 if (file_to_id_.find(key) == file_to_id_.end()) {
-                    std::vector<int> values = {1};
-                    file_to_id_[key] = values;
-                    value = 1;
                     try {
                         boost::filesystem::create_directory(path);
                     } 
@@ -41,6 +38,9 @@ status request_handler_crud::handle_request(http::request<http::string_body> req
                         response.prepare_payload();
                         return false;
                     }
+                    std::vector<int> values = {1};
+                    file_to_id_[key] = values;
+                    value = 1;
                     INFO << get_client_ip() << "Created directory for new entity: " << key << "\n";
                 }
                 //if entity already exits
@@ -81,16 +81,102 @@ status request_handler_crud::handle_request(http::request<http::string_body> req
             response.prepare_payload();
 
             return false;
-            break;
         }
         case http::verb::get:
             // TODO: handle GET request, (R)ead an entity or list all if body empty
             // response.result(http::status::ok), etc.
             break;
-        case http::verb::put:
-            // TODO: handle PUT request, (U)pdate existing entity's data
-            // status OK for update of existing entity, Created for new one, etc.
-            break;
+        case http::verb::put: {
+            std::string prefix = get_prefix() + "/";
+            size_t pos = request.target().find(prefix);
+            //if the given location is found, url starts with crud location,
+            //and the string of location is smaller than the url, this is a valid path
+            if (pos != std::string::npos && pos == 0 && request.target().length() > prefix.length()) {
+                std::string key = std::string(request.target().substr(prefix.length()));
+                int value;
+                //remove trailing slash
+                while(key.length() >= 1 && key[key.length()-1] == '/') {
+                    key.pop_back();
+                }
+                std::size_t found = key.find_last_of("/");
+                std::string id_name = key.substr(found+1);
+                std::string file_name = key.substr(0, found);
+                std::string::const_iterator it = id_name.begin();
+                while (it != id_name.end() && std::isdigit(*it)) {
+                    ++it;
+                }
+                //if after last / is integer, this is a valid path
+                if(!id_name.empty() && it == id_name.end()) {
+                    //if key before last / exists
+                    value = std::stoi(id_name);
+                    if (file_to_id_.find(file_name) != file_to_id_.end()) {
+                        std::vector<int> id_names = file_to_id_[file_name]; 
+                        if (std::find(id_names.begin(), id_names.end(), value) == id_names.end()) {
+                            id_names.push_back(value);
+                            std::vector<int> values_copy = id_names;
+                            std::sort(values_copy.begin(), values_copy.end());
+                            file_to_id_[file_name] = values_copy;
+                            response.result(http::status::created);
+                            response.body() = "Created entry at {\"id\":" + std::to_string(value) + "}" + "\n";
+                            INFO << get_client_ip() << "Created id " << std::to_string(value) << " for entity " << file_name << "\n";
+                        }
+                        else {
+                            response.result(http::status::ok);
+                            response.body() = "Updated entry at {\"id\":" + std::to_string(value) + "}" + "\n";
+                            INFO << get_client_ip() << "File updated for id " << std::to_string(value) << " for entity " << file_name << "\n";
+                        }
+                    }
+                    //if key before last / does not exist
+                    else {
+                        try {
+                            boost::filesystem::create_directory(get_dir() + '/' + file_name);
+                        } 
+                        catch (const boost::filesystem::filesystem_error& e) {
+                            INFO << get_client_ip() << "Unable to create directory for " << file_name << "\n";
+                            //unprocessable entity
+                            response.result(http::status::unprocessable_entity);
+                            response.set(http::field::content_type, "text/html");
+                            response.body() = "<html><head><title>Unprocessable Entity</title></head><body><h1>422 Unprocessable Entity</h1></body></html>";
+                            response.prepare_payload();
+                            return false;
+                        }
+                        std::vector<int> values = {value};
+                        file_to_id_[file_name] = values;
+                        response.result(http::status::created);
+                        response.body() = "Created entry at {\"id\":" + std::to_string(value) + "}" + "\n";
+                        INFO << get_client_ip() << "Created directory for new entity: " << file_name << " and created id " << std::to_string(value) << "\n";
+                    }  
+                    std::string path = get_dir() + "/" + key;
+                    std::ostringstream oss;     
+                    oss << request.body();
+                    std::string body = oss.str();
+                    oss.clear();
+                    //ofstream automatically overwrites, which is expected for PUT
+                    std::ofstream file(path);
+                    file << body;
+                    file.close();
+                    response.prepare_payload();
+                    //prepare response
+                    return true;
+                }
+                else {
+                    //else error bad request
+                    response.result(http::status::bad_request);
+                    response.set(http::field::content_type, "text/html");
+                    response.body() = "<html><head><title>Bad Request</title></head><body><h1>400 Bad Request</h1></body></html>";
+                    response.prepare_payload();
+                  
+                    return false;
+                }
+            }    
+            // cannot find the folder
+            response.result(http::status::not_found);
+            response.set(http::field::content_type, "text/html");
+            response.body() = not_found_msg;
+            response.prepare_payload();
+
+            return false;
+        }
         case http::verb::delete_:
             // TODO: handle DELETE request, (D)elete entity from db
             break;
