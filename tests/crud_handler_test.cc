fdd6bfa31ea5b6ed1ca2b00ae5294c592d2a9ef0
shammo <shammochanda@g.ucla.edu>
2022-05-17 12:26:27 -0700

    Added test for CRUD POST and PUT
    -Changed CMakeLists to include changes
    
    Change-Id: I826265416ea912e2f0cc824d8a8784c50979af40

---
+++ tests/crud_handler_test.cc
@@ -0,0 +1,182 @@
+#include "gtest/gtest.h"
+#include "config_arg.h"
+#include "request_handler_crud.h"
+
+#include <fstream>
+#include <stdio.h>
+#include <boost/filesystem.hpp>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+class CrudHandlerTest :public::testing::Test
+{
+    public:
+        CrudHandlerTest() {
+            crud_arg.root = "../crud_data";
+            crud_arg.location = "/api";
+            req_handler_crud = new request_handler_crud(crud_arg.location, crud_arg.root, file_to_id);
+        }
+    
+        ~CrudHandlerTest() {
+            delete req_handler_crud;
+        }
+    
+    
+        http::request<http::string_body> req;
+        http::response<http::string_body> rep;
+        enum {content_length_field = 0, content_type_field = 1};
+    
+        std::map<std::string, std::vector<int>> file_to_id;
+
+        config_arg crud_arg;
+        request_handler* req_handler_crud;
+};
+
+
+TEST_F(CrudHandlerTest, CrudHandlerPOSTTest)
+{
+    req.target( "/api/Shoes");
+    req.method( http::verb::post );
+    req_handler_crud -> handle_request(req, rep);
+    EXPECT_EQ(boost::filesystem::exists( "../crud_data/Shoes/1" ), true);
+    boost::filesystem::remove_all("../crud_data/Shoes");
+    EXPECT_EQ(file_to_id.count("Shoes"), true);
+    std::vector<int> ids = {1};
+    EXPECT_EQ(ids, file_to_id["Shoes"]);
+    EXPECT_EQ(rep.result(), http::status::created);
+    EXPECT_EQ(std::string(rep.body().data()), "Created entry at {\"id\":1}\n");
+}
+
+TEST_F(CrudHandlerTest, CrudHandlerSequentialPOSTTest)
+{
+    req.target( "/api/Shoes");
+    req.method( http::verb::post );
+    req_handler_crud -> handle_request(req, rep);
+    req_handler_crud -> handle_request(req, rep);
+    EXPECT_EQ(boost::filesystem::exists( "../crud_data/Shoes/1" ), true);
+    EXPECT_EQ(boost::filesystem::exists( "../crud_data/Shoes/2" ), true);
+    boost::filesystem::remove_all("../crud_data/Shoes");
+    EXPECT_EQ(file_to_id.count("Shoes"), true);
+    std::vector<int> ids = {1, 2};
+    EXPECT_EQ(ids, file_to_id["Shoes"]);
+    EXPECT_EQ(rep.result(), http::status::created);
+    EXPECT_EQ(std::string(rep.body().data()), "Created entry at {\"id\":2}\n");
+}
+
+TEST_F(CrudHandlerTest, CrudHandlerPOSTDirCreationFailTest)
+{
+    req.target( "/api/Shoes");
+    req.method( http::verb::post );
+    req_handler_crud -> handle_request(req, rep);
+    req.target( "/api/Shoes/1");
+    req_handler_crud -> handle_request(req, rep);
+    EXPECT_EQ(boost::filesystem::exists( "../crud_data/Shoes/1" ), true);
+    EXPECT_EQ(boost::filesystem::exists( "../crud_data/Shoes/1/1" ), false);
+    boost::filesystem::remove_all("../crud_data/Shoes");
+    EXPECT_EQ(file_to_id.count("Shoes"), true);
+    std::vector<int> ids = {1};
+    EXPECT_EQ(ids, file_to_id["Shoes"]);
+    EXPECT_EQ(rep.result(), http::status::unprocessable_entity);
+    EXPECT_EQ(std::string(rep.body().data()), "<html><head><title>Unprocessable Entity</title></head><body><h1>422 Unprocessable Entity</h1></body></html>");
+}
+
+TEST_F(CrudHandlerTest, CrudHandlerPOSTSmallestIDTest)
+{
+    file_to_id["Shoes"] = {2};
+    boost::filesystem::create_directory("../crud_data/Shoes");
+    std::ofstream file("../crud_data/Shoes/2");
+    file << "test";
+    file.close();
+    request_handler* req_handler_crud_1 = new request_handler_crud(crud_arg.location, crud_arg.root, file_to_id);
+    EXPECT_EQ(boost::filesystem::exists( "../crud_data/Shoes/1" ), false);
+    req.target( "/api/Shoes");
+    req.method( http::verb::post );
+    req_handler_crud_1 -> handle_request(req, rep);
+    EXPECT_EQ(boost::filesystem::exists( "../crud_data/Shoes/1" ), true);
+    EXPECT_EQ(boost::filesystem::exists( "../crud_data/Shoes/2" ), true);
+    boost::filesystem::remove_all("../crud_data/Shoes");
+    EXPECT_EQ(file_to_id.count("Shoes"), true);
+    std::vector<int> ids = {1, 2};
+    EXPECT_EQ(ids, file_to_id["Shoes"]);
+    EXPECT_EQ(rep.result(), http::status::created);
+    EXPECT_EQ(std::string(rep.body().data()), "Created entry at {\"id\":1}\n");
+    delete req_handler_crud_1;
+}
+
+TEST_F(CrudHandlerTest, CrudHandlerPUTInvalidFileTest)
+{
+    req.target( "/api/Shoes/a");
+    req.method( http::verb::put );
+    req_handler_crud -> handle_request(req, rep);
+    EXPECT_EQ(rep.result(), http::status::bad_request);
+}
+
+TEST_F(CrudHandlerTest, CrudHandlerPUTNewEntityTest)
+{
+    req.target( "/api/Shoes/2");
+    req.method( http::verb::put );
+    req_handler_crud -> handle_request(req, rep);
+    EXPECT_EQ(boost::filesystem::exists( "../crud_data/Shoes/2" ), true);
+    boost::filesystem::remove_all("../crud_data/Shoes");
+    EXPECT_EQ(file_to_id.count("Shoes"), true);
+    std::vector<int> ids = {2};
+    EXPECT_EQ(ids, file_to_id["Shoes"]);
+    EXPECT_EQ(rep.result(), http::status::created);
+    EXPECT_EQ(std::string(rep.body().data()), "Created entry at {\"id\":2}\n");
+}
+
+TEST_F(CrudHandlerTest, CrudHandlerPUTDirCreationFailTest)
+{
+    req.target( "/api/Shoes/1");
+    req.method( http::verb::put );
+    req_handler_crud -> handle_request(req, rep);
+    req.target( "/api/Shoes/1/1");
+    req_handler_crud -> handle_request(req, rep);
+    EXPECT_EQ(boost::filesystem::exists( "../crud_data/Shoes/1/1" ), false);
+    EXPECT_EQ(boost::filesystem::exists( "../crud_data/Shoes/1" ), true);
+    boost::filesystem::remove_all("../crud_data/Shoes");
+    EXPECT_EQ(file_to_id.count("Shoes"), true);
+    std::vector<int> ids = {1};
+    EXPECT_EQ(ids, file_to_id["Shoes"]);
+    EXPECT_EQ(rep.result(), http::status::unprocessable_entity);
+    EXPECT_EQ(std::string(rep.body().data()), "<html><head><title>Unprocessable Entity</title></head><body><h1>422 Unprocessable Entity</h1></body></html>");
+}
+
+TEST_F(CrudHandlerTest, CrudHandlerPUTNewFileExistingEntityTest)
+{
+    req.target( "/api/Shoes/1");
+    req.method( http::verb::put );
+    req_handler_crud -> handle_request(req, rep);
+    req.target( "/api/Shoes/2");
+    req_handler_crud -> handle_request(req, rep);
+    EXPECT_EQ(boost::filesystem::exists( "../crud_data/Shoes/2" ), true);
+    EXPECT_EQ(boost::filesystem::exists( "../crud_data/Shoes/1" ), true);
+    boost::filesystem::remove_all("../crud_data/Shoes");
+    EXPECT_EQ(file_to_id.count("Shoes"), true);
+    std::vector<int> ids = {1, 2};
+    EXPECT_EQ(ids, file_to_id["Shoes"]);
+    EXPECT_EQ(rep.result(), http::status::created);
+    EXPECT_EQ(std::string(rep.body().data()), "Created entry at {\"id\":2}\n");
+}
+
+TEST_F(CrudHandlerTest, CrudHandlerPUTExistingFileTest)
+{
+    req.target( "/api/Shoes/1");
+    req.method( http::verb::put );
+    req_handler_crud -> handle_request(req, rep);
+    req.target( "/api/Shoes/1");
+    req_handler_crud -> handle_request(req, rep);
+    EXPECT_EQ(boost::filesystem::exists( "../crud_data/Shoes/1" ), true);
+    boost::filesystem::remove_all("../crud_data/Shoes");
+    EXPECT_EQ(file_to_id.count("Shoes"), true);
+    std::vector<int> ids = {1};
+    EXPECT_EQ(ids, file_to_id["Shoes"]);
+    EXPECT_EQ(rep.result(), http::status::ok);
+    EXPECT_EQ(std::string(rep.body().data()), "Updated entry at {\"id\":1}\n");
+}
+
+
+
+
